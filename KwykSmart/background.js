4ff685e6f9ce43d83e4beae2a06960d7dc8a6390
Gametuto <gametutoytb@gmail.com>
2024-09-17 19:26:24 +0200

    gérer les messages de copie

--- KwykSmart/background.js
+++ KwykSmart/background.js
@@ -1,14 +1,17 @@
+// background.js
 chrome.runtime.onInstalled.addListener(() => {
   console.log('KwykSmart extension installed');
 });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log('Message received in background:', message);
-  sendResponse();
-});
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.status) {
-    console.log('Status received:', request.status);
+  if (request.action === 'copy') {
+    const mode = request.mode;
+    const tabId = sender.tab.id;
+    chrome.tabs.sendMessage(tabId, {action: 'copy', mode: mode}, function(response) {
+      sendResponse(response);
+    });
+  } else if (request.action === 'getStatus') {
+    // Logique pour obtenir le statut (facultatif)
+    sendResponse({status: 'Statut obtenu'});
   }
 });
